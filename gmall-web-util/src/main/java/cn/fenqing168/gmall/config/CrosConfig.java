c11363dfb94ef410efce8f5db63c7a3e41cdd17f
fenqing <1>
2020-03-01 05:53:43 +0800

    .更新git忽略

---
+++ gmall-web-util/src/main/java/cn/fenqing168/gmall/config/CrosConfig.java
@@ -0,0 +1,23 @@
+package cn.fenqing168.gmall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author fenqing
+ */
+@Configuration
+public class CrosConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+
+}
